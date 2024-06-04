0ffa173cc37e6197893959a47b48ad38c40971f2
Mohammad-Ismail-DEV <mohamad.ismail.dev@gmail.com>
2024-06-05 00:20:10 +0300

    login, home views done with backend

--- src/app.js
+++ src/app.js
@@ -5,6 +5,22 @@ const app = express();
 const port = 3000;
 var connection = require("./database");
 const session = require("express-session");
+require("dotenv").config();
+
+const { google } = require("googleapis");
+const youtube = google.youtube("v3");
+
+const {
+  GOOGLE_API_KEY,
+  CLIENT_ID,
+  CLIENT_SECRET,
+} = require("./utils/environment");
+
+const oauth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  "http://localhost:3000"
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,15 +39,27 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.get("/", function (req, res) {
+app.get("/", async (req, res) => {
   const user = req.session.user;
-  const error = req.query.error;
+  const { code, error } = req.query;
   const errorMessages = {
     wrongCred: "Wrong username or password!",
     serverError: "Server Error!",
     missing: "Missing Credentials!",
   };
   if (user) {
+    if (code) {
+      const {
+        tokens: { access_token, refresh_token },
+      } = await oauth2Client.getToken(code);
+      req.session.user.access_token = access_token;
+      req.session.user.refresh_token = refresh_token;
+      connection.query(
+        `Update users set access_token = ?, refresh_token = ? WHERE id = ?`,
+        [access_token, refresh_token, user.id]
+      );
+    }
+
     return res.redirect("/home");
   }
   res.render("pages/login", { errorMessage: errorMessages[error], user });
@@ -45,26 +73,68 @@ const authenticatedRoute = (req, res, next) => {
   next();
 };
 
-app.get("/home", authenticatedRoute, (req, res) => {
-  const user = req.session.user;
-  res.render("pages/home", { user });
+app.get("/home", authenticatedRoute, async (req, res) => {
+  const { user } = req.session;
+  const { access_token, refresh_token } = user;
+  let channel = {};
+  let videos = [];
+  let linked = false;
+
+  if (access_token && refresh_token) {
+    linked = true;
+
+    oauth2Client.setCredentials({
+      access_token,
+      refresh_token,
+    });
+
+    const response = await youtube.channels.list({
+      part: "snippet",
+      access_token: access_token,
+      mine: true,
+    });
+    const channels = response.data.items.map((item) => ({
+      id: item.id,
+      title: item.snippet.title,
+    }));
+    channel = channels[0];
+
+    console.log("channel", channel);
+
+    if (channel) {
+      const response = await youtube.playlistItems.list({
+        part: "snippet",
+        access_token: access_token,
+        playlistId: "UU" + channel.id.slice(2),
+      });
+
+      videos = response.data.items.map((item) => ({
+        id: item.id,
+        resourceId: item.snippet.resourceId.videoId,
+        title: item.snippet.title,
+        thumbnail: item.snippet.thumbnails.standard.url,
+      }));
+    }
+  }
+
+  res.render("pages/home", { user, channel, videos, linked });
 });
 
 app.get("/register", function (req, res) {
   res.render("pages/register");
 });
 
-app.post("/home", (req, res) => {
+app.post("/login", (req, res) => {
   if (req.body.user_name && req.body.password) {
     connection.query(
-      `Select id, user_name from users where user_name=? && password=?`,
+      `Select id, user_name, access_token, refresh_token from users where user_name=? && password=?`,
       [req.body.user_name.toLowerCase(), req.body.password],
       (error, result) => {
         if (error) {
           res.redirect("/?error=serverError");
         } else {
           if (result.length > 0) {
-            req.session.user = result[0].id;
+            req.session.user = result[0];
             res.redirect("/home");
           } else {
             res.redirect("/?error=wrongCred");
@@ -128,24 +198,28 @@ app.post("/home", (req, res) => {
 //   }
 // });
 
+app.get("/youtube", authenticatedRoute, async (req, res) => {
+  const scopes = [
+    "https://www.googleapis.com/auth/youtube",
+    "https://www.googleapis.com/auth/youtube.force-ssl",
+    "https://www.googleapis.com/auth/youtube.readonly",
+    "https://www.googleapis.com/auth/youtubepartner",
+    "https://www.googleapis.com/auth/youtubepartner-channel-audit",
+  ];
 
+  const url = oauth2Client.generateAuthUrl({
+    // 'online' (default) or 'offline' (gets refresh_token)
+    access_type: "offline",
 
-app.get("/youtube", authenticatedRoute, async(req, res)=>{
-  const auth = new google.auth.GoogleAuth({
-    // Scopes can be specified either as an array or as a single, space-delimited string.
-    scopes: [
-      'https://www.googleapis.com/auth/youtube',
-      'https://www.googleapis.com/auth/youtube.force-ssl',
-      'https://www.googleapis.com/auth/youtube.readonly',
-      'https://www.googleapis.com/auth/youtubepartner',
-      'https://www.googleapis.com/auth/youtubepartner-channel-audit',
-    ],
+    // If you only need one scope you can pass it as a string
+    scope: scopes,
   });
+  res.redirect(url);
 
   // Acquire an auth client, and bind it to all future calls
-  const authClient = await auth.getClient();
-  google.options({auth: authClient});
-})
+  // const authClient = await auth.getClient();
+  // google.options({ auth: authClient, key: GOOGLE_API_KEY });
+});
 
 app.get("/logout", (req, res) => {
   req.session.destroy((err) => {
---
+++ src/utils/environment.js
@@ -0,0 +1,7 @@
+const { GOOGLE_API_KEY, CLIENT_ID, CLIENT_SECRET } = process.env;
+
+module.exports = {
+  GOOGLE_API_KEY,
+  CLIENT_ID,
+  CLIENT_SECRET,
+};
