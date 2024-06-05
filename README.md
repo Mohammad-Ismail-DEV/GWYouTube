285ad2eb8ebccfe931622e12bc8b60d400d2672c
Mohammad-Ismail-Dev <mohamad.ismail.dev@gmail.com>
2024-06-05 15:04:59 +0300

    added readme file

---
+++ README.md
@@ -0,0 +1,11 @@
+# GW YouTube
+
+## How to run:
+
+- create `gwyoutube` database.
+- import sql file (included in repository).
+- install node packages using nodeJs v16.13.1 : `npm install`.
+- create `.env` file from `.env.example` as a template.
+- get needed API keys and secrets from google console and set them in `.env` file.
+- run the application: `npm run dev`.
+- access the application on `http://localhost:3000`
--- src/database.js
+++ src/database.js
@@ -1,17 +1,17 @@
-var mysql = require("mysql")
+var mysql = require("mysql");
 var connection = mysql.createConnection({
-    multipleStatements: true,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "GWYouTube",
-    port: 3306
-})
+  multipleStatements: true,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "gwyoutube",
+  port: 3306,
+});
 connection.connect((err) => {
-    if (err) {
-        console.log(err)
-        return
-    }
-    console.log("Database connected")
-})
-module.exports = connection
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("Database connected");
+});
+module.exports = connection;
